756c194a616e5082b8d89fa9d36a0ae169d6906b
dokaben1003 <aki2262hiro103kawano@gmail.com>
2024-08-14 18:31:17 +0900

    release 0.01

--- setup.py
+++ setup.py
@@ -6,6 +6,10 @@ def get_requirements_from_file():
         requirements = f_in.read().splitlines()
     return requirements
 
+def get_long_description():
+    with open("README.md", "r", encoding="utf-8") as fh:
+        return fh.read()
+
 
 setup(
     name="meaPolygon",
@@ -13,6 +17,14 @@ setup(
     author="dokaben1003",
     author_email="aki2262hiro103kawano@gmail.com",
     description="This repository provides a tool to calculate the area of polygons in semantic segmentation data, specifically for images in YOLO format. It enables the precise calculation of areas for objects defined as polygons within images. This tool is useful for researchers and developers in analyzing and evaluating segmentation tasks.",
+    long_description=get_long_description(),  # README.md から説明文を取得
+    long_description_content_type="text/markdown",  # README.md が Markdown フォーマットであることを指定
     package_dir={"": "src"},
-    install_requires=get_requirements_from_file()
+    install_requires=get_requirements_from_file(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
 )
